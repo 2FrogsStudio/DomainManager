5642e436531adb07d17f0b9865706de0f1c84ef9
Alexander Salamatov <ch1seL@ya.ru>
2023-05-03 19:52:37 +0300

    issuer can be null

---
+++ src/DomainManager.Database/Migrations/20230503165219_IssuerCanBeNull.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DomainManager.Migrations
+{
+    /// <inheritdoc />
+    public partial class IssuerCanBeNull : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Issuer",
+                table: "SslMonitor",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Issuer",
+                table: "SslMonitor",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+    }
+}
--- src/DomainManager.Database/Models/SslMonitor.cs
+++ src/DomainManager.Database/Models/SslMonitor.cs
@@ -6,7 +6,7 @@ public class SslMonitor {
     public int Id { get; set; }
     public string Domain { get; set; }
     public DateTime? LastUpdateDate { get; set; }
-    public string Issuer { get; set; }
+    public string? Issuer { get; set; }
     public DateTime NotAfter { get; set; }
     public DateTime NotBefore { get; set; }
     public SslPolicyErrors Errors { get; set; }
