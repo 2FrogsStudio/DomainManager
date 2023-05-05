843e5f7279236bc8d93bb336d943cb155f9a0782
Alexander Salamatov <ch1seL@ya.ru>
2023-05-05 15:32:08 +0300

    add sentry

--- src/DomainManager/HostApplicationBuilderExtensions.cs
+++ src/DomainManager/HostApplicationBuilderExtensions.cs
@@ -4,13 +4,14 @@ namespace DomainManager;
 
 public static class HostApplicationBuilderExtensions {
     public static HostApplicationBuilder AddLogging(this HostApplicationBuilder builder) {
+        // fix sentry environment
+        Environment.SetEnvironmentVariable("SENTRY_ENVIRONMENT", builder.Environment.EnvironmentName);
+
         builder.Services.AddLogging(loggingBuilder => {
             loggingBuilder.ClearProviders();
-            var logger = new LoggerConfiguration()
+            loggingBuilder.AddSerilog(new LoggerConfiguration()
                 .ReadFrom.Configuration(builder.Configuration)
-                .Enrich.FromLogContext()
-                .CreateLogger();
-            loggingBuilder.AddSerilog(logger);
+                .CreateLogger());
         });
         return builder;
     }
