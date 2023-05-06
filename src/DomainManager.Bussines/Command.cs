30f93fd8617193b6378bc42680a2031686d2aa3f
Alexander Salamatov <ch1seL@ya.ru>
2023-05-06 16:02:14 +0300

    fix spelling

--- src/DomainManager.Bussines/Command.cs
+++ src/DomainManager.Bussines/Command.cs
@@ -14,7 +14,7 @@ public enum Command {
                "`/domain_monitor list` - get list of your domains to monitor\n" +
                "`/domain_monitor add` `domain.com` - add domain to monitoring\n" +
                "`/domain_monitor delete` `domain.com` - delete domain from monitoring\n" +
-               "  [*] to add or delete domain you should be in chat admins"
+               "  [*] to add or delete domain you should be a chat admins"
     )]
     DomainMonitor,
 
@@ -24,7 +24,7 @@ public enum Command {
                "`/ssl_monitor list` - get list of your hosts to monitor\n" +
                "`/ssl_monitor add` `my.site.com` - add host to monitoring\n" +
                "`/ssl_monitor delete` `my.site.com` - delete host from monitoring\n" +
-               "  [*] to add or delete host you should be in chat admins"
+               "  [*] to add or delete host you should be a chat admins"
     )]
     SslMonitor,
 
@@ -36,7 +36,7 @@ public enum Command {
                "`/schedule run` - force run updating job\n" +
                "`/schedule status` - get job status\n" +
                "  [cron_expr] `0 0 12 ? * 2-6 *` - fire monitoring job every 12 hours from monday to friday\n" +
-               "  [*] to control job you should be in chat admins"
+               "  [*] to control job you should be a chat admins"
     )]
     Schedule
 }
