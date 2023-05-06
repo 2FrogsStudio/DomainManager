ec59425b955c6ffedc854dbaa8b995ce966b0971
Alexander Salamatov <ch1seL@ya.ru>
2023-05-06 10:44:47 +0300

    added possibility to local run

--- src/DomainManager.Bussines/Configuration/BotOptions.cs
+++
@@ -1,5 +0,0 @@
-namespace DomainManager.Configuration;
-
-public class BotOptions {
-    public string Token { get; init; } = "YOUR_ACCESS_TOKEN_HERE"!;
-}
--- src/DomainManager.Bussines/HostApplicationBuilderExtensions.cs
+++ src/DomainManager.Bussines/HostApplicationBuilderExtensions.cs
@@ -1,5 +1,4 @@
 using DomainManager.Abstract;
-using DomainManager.Configuration;
 using DomainManager.Jobs;
 using DomainManager.Notifications.UpdateConsumers;
 using DomainManager.Services;
@@ -7,7 +6,6 @@ using MassTransit;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Options;
 using Quartz;
 using Telegram.Bot;
 using Telegram.Bot.Polling;
@@ -17,18 +15,14 @@ namespace DomainManager;
 
 public static class HostApplicationBuilderExtensions {
     public static HostApplicationBuilder AddBusiness(this HostApplicationBuilder builder) {
-        builder.Services.Configure<BotOptions>(builder.Configuration.GetSection("Bot"));
-
+        var botToken = builder.Configuration["Bot:Token"];
+        if (botToken is null or "BOT_API_TOKEN_HERE") {
+            throw new NullReferenceException(
+                "Provide token of your Telegram bot with `Bot__Token` environment variable");
+        }
         builder.Services.AddMemoryCache();
         builder.Services.AddHttpClient("TelegramBotClient")
-            .AddTypedClient<ITelegramBotClient>((client, provider) => {
-                var botToken = provider.GetRequiredService<IOptions<BotOptions>>().Value.Token;
-                if (botToken is null or "YOUR_ACCESS_TOKEN_HERE") {
-                    throw new NullReferenceException("Bot:Token should be provided");
-                }
-
-                return new TelegramBotClient(botToken, client);
-            });
+            .AddTypedClient<ITelegramBotClient>(client => new TelegramBotClient(botToken, client));
         builder.Services.AddQuartz(q => {
             q.UseMicrosoftDependencyInjectionJobFactory();
             q.UsePersistentStore(s => {
