cfdb31d2f49a7767775a2c1e6411296ec119491d
Alexander Salamatov <ch1seL@ya.ru>
2023-05-10 10:47:46 +0300

    log host ip

--- src/DomainManager.Bussines/Notifications/CommandConsumers/PingCommandConsumer.cs
+++ src/DomainManager.Bussines/Notifications/CommandConsumers/PingCommandConsumer.cs
@@ -1,4 +1,6 @@
+using System.Net;
 using System.Net.NetworkInformation;
+using System.Text.Json;
 using DomainManager.Abstract;
 using DomainManager.Notifications.CommandConsumers.Base;
 using Microsoft.Extensions.Caching.Memory;
@@ -7,24 +9,37 @@ using Telegram.Bot;
 namespace DomainManager.Notifications.CommandConsumers;
 
 public class PingCommandConsumer : CommandConsumerBase, IMediatorConsumer {
-    public PingCommandConsumer(ITelegramBotClient botClient, IMemoryCache memoryCache) :
-        base(Command.PingCommand, botClient, memoryCache) { }
+    private readonly ILogger<PingCommandConsumer> _logger;
+
+    public PingCommandConsumer(ITelegramBotClient botClient, IMemoryCache memoryCache,
+        ILogger<PingCommandConsumer> logger) :
+        base(Command.PingCommand, botClient, memoryCache) {
+        _logger = logger;
+    }
 
     protected override async Task<string> Consume(string[] args, Message message, long chatId, bool isAdmin,
         CancellationToken cancellationToken) {
         return args switch {
-            [{ } host] => await PingCommand(host),
+            [{ } host] => await PingCommand(host, cancellationToken),
             _ => CommandHelpers.CommandAttributeByCommand[Command.PingCommand]!.Help!
         };
     }
 
-    private static async Task<string> PingCommand(string host) {
+    private async Task<string> PingCommand(string host, CancellationToken cancellationToken) {
+        _logger.LogInformation("Ping host: {Host}", host);
+        var hostIps = await Dns.GetHostAddressesAsync(host, cancellationToken);
+        _logger.LogInformation("Host: {Host} IPs: {HostIps}", host,
+            JsonSerializer.Serialize(hostIps.Select(ip => ip.ToString())));
+        if (hostIps is not [var hostIp, ..]) {
+            return "IP Address was not resolved";
+        }
+        PingReply reply;
         using var ping = new Ping();
         try {
-            var reply = await ping.SendPingAsync(host, 5000);
-            return $"Status :  {reply.Status:G} \n Latency : {reply.RoundtripTime}ms \n Address : {reply.Address}";
+            reply = await ping.SendPingAsync(hostIp, 5000);
         } catch (PingException e) {
             return "Exception occurred: " + e.Message;
         }
+        return $"Status :  {reply.Status:G} \n Latency : {reply.RoundtripTime}ms \n Address : {reply.Address}";
     }
 }
